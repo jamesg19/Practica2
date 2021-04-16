96cf2f940481f8a62010c996a4465a31e3322b23
james <jamesosmin-gramajocarcamo@cunoc.edu.gt>
2021-04-16 11:51:51 -0400

    parser

--- Practica2/src/assets/prueba.js
+++ Practica2/src/assets/prueba.js
@@ -4,11 +4,14 @@ var parser = require("./basico").parser;
 function compilar(){
   //leo el text area donde esta el la gramatica ingresada
   var entr = document.getElementById('area');
+  const respuesta = document.getElementById('respuesta');
   //envio la entrada al parser
   try {
-     parser.parser(entr.value);
+     parser=parser(entr.value);
+    
   } catch (e) {
-    alert('error :(');
+    //alert('error :(');
+    respuesta.value= e; 
     console.error(e);
     return;
   }
